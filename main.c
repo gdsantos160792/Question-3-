59e9bfd9d2643fd4a00297eca8aec04044141ffd
Gabriel dos Santos <81043470+gdsantos160792@users.noreply.github.com>
2021-08-15 23:53:57 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* This file has the solution for the questions 3. This progrom 
+
+    const char * ConcatRemove(char s[100], char t[100], int k)
+    {
+        //Start the solution for problem 4//
+
+        if(strlen(s)>100 || strlen(t)>100 || k>100 ||k<0){
+            return "The entered data is out off the range values";
+        }
+
+        //Finish solution for problem 4 //
+        if((strlen(s)+strlen(t))<k)
+
+            {
+                return "Yes";
+                }
+        //Here we will find the common letters of the strings//
+
+        int cont = 0;
+        int low_value;
+
+        if(strlen(s)<strlen(t)){low_value=strlen(s);}
+
+        else{low_value=strlen(t);}
+
+        for (int i = 0;i < low_value;i++){
+            if (s[i]==t[i]){
+                cont++;
+            }
+            else{
+                break;
+            }
+        }
+        if (k>=(strlen(s)+strlen(t)-2*cont)){
+                return "Yes";
+            }
+        else
+            {
+                return "No";
+                }
+    }
+
+int main(void)
+{
+
+    printf("%s",ConcatRemove("Test","Gabriel",5));
+}
